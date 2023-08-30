933f47a322ea3fb25220e597f39f04a47aaff881
mercur987 <109650285+mercur987@users.noreply.github.com>
2023-08-30 09:10:33 +0900

    Update main.cpp

--- Sample_02_02/Sample_02_02/main.cpp
+++ Sample_02_02/Sample_02_02/main.cpp
@@ -1,6 +1,6 @@
 ﻿#include "stdafx.h"
 #include "system/system.h"
-
+using namespace std;
 // 頂点構造体
 struct SimpleVertex
 {
@@ -30,8 +30,8 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
 
     // 2. シェーダーをロード
     Shader vs, ps;
-    vs.LoadVS("Assets/shader/sample.fx", "VSMain");
-    ps.LoadPS("Assets/shader/sample.fx", "PSMain");
+    vs.LoadVS("Assets/shader/sample.after_4.fx", "VSMain");
+    ps.LoadPS("Assets/shader/sample.after_4.fx", "PSMain");
 
     // 3. パイプラインステートを作成
     PipelineState pipelineState;
@@ -39,7 +39,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
 
     // 4. 三角形の頂点バッファを作成
     // 頂点配列を定義
-    SimpleVertex vertices[] = {
+    /*SimpleVertex vertices[] = {
         {
             {-0.5f, -0.5f, 0.0f},
             { 1.0f, 0.0f, 0.0f }
@@ -52,7 +52,22 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
             { 0.5f, -0.5f, 0.0f },
             { 0.0f, 0.0f, 1.0f }
         }
-    };
+    };*/
+    // 頂点配列を定義
+    const  int POLYGONAL_NUMBER = 17;	//　多角形の頂点数
+    const float POLYGON_RADIUS = 0.75f;	// 外接円の半径
+    const double M_PI = 3.14159265358979323846;
+    SimpleVertex vertices[POLYGONAL_NUMBER];
+    for (int i = 0; i < POLYGONAL_NUMBER; i++) {
+        vertices[i].pos[0] = cos(2 * M_PI * i / POLYGONAL_NUMBER) * (float)POLYGON_RADIUS * 9.0f / 16.0f;
+        vertices[i].pos[1] = sin(2 * M_PI * i / POLYGONAL_NUMBER) * POLYGON_RADIUS;
+        vertices[i].pos[2] = 0;
+        vertices[i].color[0] = cos(2 * M_PI * i / POLYGONAL_NUMBER);
+        vertices[i].color[1] = sin(2 * M_PI * i / POLYGONAL_NUMBER);
+        vertices[i].color[2] = (float)i / POLYGONAL_NUMBER;
+        //vertices[i].color[3] = 1.0f;
+    }
+
 
     VertexBuffer triangleVB;
     triangleVB.Init(sizeof(vertices), sizeof(vertices[0]));
@@ -60,9 +75,16 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
 
     // 5. 三角形のインデックスバッファを作成
     //インデックス配列
-    uint16_t indices[] = {
+    /*uint16_t indices[] = {
         0,1,2
     };
+    */
+    uint16_t indices[(POLYGONAL_NUMBER - 2) * 3];
+    for (int i = 0; i < _countof(indices); i += 3) {
+        indices[i] = 0;
+        indices[i + 1] = (i / 3) + 1;
+        indices[i + 2] = (i / 3) + 2;
+    }
     IndexBuffer triangleIB;
     triangleIB.Init(sizeof(indices), 2);
     triangleIB.Copy(indices);
